b6b99885c8c34070173ff050aa37540d8c2013cc
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-19 17:26:13 +0600

    first commit

---
+++ Constractor & __init__.py
@@ -0,0 +1,14 @@
+class Phone:
+    manufactured = 'China'
+
+    def __init__(self, owner, brand, price):
+        self.owner = owner
+        self.brand = brand
+        self.price = price
+
+    def sendSMS(self, phone, sms):
+        text = f'Sending to: {phone} {sms}'
+        print(text)
+
+myPhone = Phone('Sanim', 'Samsung', 16000)
+print(myPhone.owner, myPhone.brand, myPhone.price)
